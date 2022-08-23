b70216623fa7a26b251344873a663c6424d57c5a
root <root@tv.lan>
2022-08-23 09:48:22 +0300

    delete-all

--- 1.lua
+++
@@ -1,4 +0,0 @@
-
-local string1 = "proverka svazy {{killme}} poka"
-print(string.gsub(string1, "{{killme}}", "kuku"))
-print(string1)
--- ntv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /media
-URL=`grep -A4 НТВ /root/main.m3u|grep -v '^#'| head -1`
-echo URL $URL
-echo $URL > /root/now_playing.url
-
